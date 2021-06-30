eb7058ee6ec579a8be218f298ca0a4a3b5f4da91
Alieu Baldeh <alieu.dev@gmail.com>
2021-06-29 23:00:06 -0400

    commit tree d82b01bf3cf89c1e3d2b90f1be2f5cb46c4dcf0a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(array, driver) {
+    return array.filter(elem => elem.toLowerCase() === driver.toLowerCase())
+}
+
+function fuzzyMatch(array, letters) {
+    let len = letters.length;
+    return array.filter(name => name.slice(0,len) == letters)
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string)
+}
